d58ae0372d60614772d15a047b84cc584f94797c
Paşa İSMİHAN <119595276+pasaismihan@users.noreply.github.com>
2023-10-10 11:52:34 +0300

    jqery ile client side olusturuldu

---
+++ SignalRChatServer/SignalRChatServer/Modals/Client.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalRChatServer.Modals
+{
+    public class Client
+    {
+        public string ConnectionId { get; set; }
+        public string NickName { get; set; }
+    }
+}
--- SignalRChatServer/SignalRChatServer/Program.cs
+++ SignalRChatServer/SignalRChatServer/Program.cs
@@ -1,22 +1,32 @@
+using SignalRChatServer.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddCors(option => option.AddDefaultPolicy(policy => policy.AllowCredentials()
+.AllowAnyHeader()
+.AllowAnyOrigin()
+.SetIsOriginAllowed(x => true)
+)) ;
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseCors();
 app.UseRouting();
+app.UseEndpoints(endpoints =>
+endpoints.MapHub<ChatHub>("chathub"));
 
 app.UseAuthorization();
 
